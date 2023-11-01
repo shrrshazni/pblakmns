6b0af4246031a8998044de677956cc0237ecc719
Sharir Shazni Sazali <shrrshazni@gmail.com>
2023-11-01 23:18:55 +0800

    Frontend added for user(staff)

--- app.js
+++ app.js
@@ -16,11 +16,107 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// STARTUP
+//init session
+app.use(
+  session({
+    secret: 'Our little secret.',
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+//init passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//init mongoose
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/pblakmnsDB');
+}
+
+//init mongoose items
+
+const userSchema = new mongoose.Schema({
+  fullname: String,
+  password: String,
+  role: String,
+  email: String
+});
+
+//mongoose passport-local
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+//mongoose model
+const User = new mongoose.model('User', userSchema);
+
+//simplified passport-local-mongoose
+passport.use(User.createStrategy());
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  const user = User.findById(id);
+  if (!user) {
+    done(null, false);
+  } else {
+    done(null, user);
+  }
+});
+
+// SIGN IN
 
 //init index
 app.get('/', function (req, res) {
   res.render('sign-in');
+});
+
+app.post('/', async function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = new User({
+    username: username,
+    password: password
+  });
+
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate('local')(req, res, function () {
+        res.redirect('/home');
+      });
+    }
+  });
+});
+
+//SIGN UP
+
+app.get('/sign-up', function (req, res) {
+  res.render('sign-up');
+});
+
+app.post('/sign-up', function (req, res) {
+  //pasport local mongoose
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect('/sign-up');
+      } else {
+        passport.authenticate('local')(req, res, function () {
+          res.redirect('/sign-in');
+        });
+      }
+    }
+  );
 });
 
 //init forgot pasword
@@ -73,6 +169,17 @@ app.get('/schedule', function (req, res) {
   res.render('schedule');
 });
 
+//PROFILE
+
+app.get('/profile', function (req, res) {
+  res.render('profile');
+});
+
+//SETTINGS
+
+app.get('/settings', function (req, res) {
+  res.render('settings');
+});
 //check server
 app.listen(3000, function () {
   console.log('Server started on port 3000.');
